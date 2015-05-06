c0bfcd03136b3c99ce8e036d9b40b76a8d2f3941
eykd <david.eyk@gmail.com>
2015-05-06 13:59:45 -0700

    BOOM.

---
+++ scriptter.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Scriptter
+
+Usage:
+    scriptter [--reset] [--state <state-path>] [trial | run] <schedule>
+    scriptter check <schedule>
+
+Options:
+    -h --help              Show this screen.
+    --version              Show version.
+    --verbose              Show verbose output.
+    --state <state-path>   Path for storing state [default: "./state.yml"]
+    --reset                Reset stored state
+"""
+from collections import deque, Iterable, OrderedDict
+import datetime as dt
+import itertools as it
+import logging
+import pprint
+import shlex
+import subprocess
+import uuid
+
+from docopt import docopt
+from path import path
+import parsedatetime
+import pytz
+import six
+import yaml
+
+VERSION = 0.1
+
+logger = logging.getLogger('scriptter')
+
+# __all__ = ['Schedule']
+
+
+# Ordered loading/dumping of YAML: http://stackoverflow.com/a/21912744/18950
+class OrderedLoader(yaml.Loader):
+    pass
+
+
+def construct_mapping(loader, node):
+    loader.flatten_mapping(node)
+    return OrderedDict(loader.construct_pairs(node))
+OrderedLoader.add_constructor(
+    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+    construct_mapping)
+
+
+class OrderedDumper(yaml.Dumper):
+    pass
+
+
+def _dict_representer(dumper, data):
+    return dumper.represent_mapping(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        data.items())
+OrderedDumper.add_representer(OrderedDict, _dict_representer)
+
+
+def _load_yaml(method, data):
+    result = getattr(yaml, method)(data, Loader=OrderedLoader)
+    if method == 'load_all':
+        result = deque(result)
+    return result
+
+
+def _load_file_path_or_yaml(method, data):
+    if isinstance(data, six.string_types):
+        if len(data.strip().splitlines()) > 1:
+            return _load_yaml(method, data)
+        else:
+            file_path = path(data)
+            if file_path.exists():
+                with open(data) as fi:
+                    return _load_yaml(method, fi)
+
+    else:
+        return _load_yaml(method, data)
+
+
+def yaml_load(data):
+    result = _load_file_path_or_yaml('load', data)
+    return result if result is not None else {}
+
+
+def yaml_load_all(data):
+    result = _load_file_path_or_yaml('load_all', data)
+    return result if result is not None else deque()
+
+
+def yaml_dump(data):
+    return yaml.dump(data, Dumper=OrderedDumper, default_flow_style=False)
+
+
+def yaml_dump_all(data):
+    return yaml.dump_all(data, Dumper=OrderedDumper, default_flow_style=False)
+
+
+DEFAULTS = {
+    'delay': '5m',
+    'timezone': 'US/Pacific',
+    'cmd': 'echo @{as} says: {say}',
+    'repeat': True,
+}
+
+
+class ScheduleLoader(object):
+    def __init__(self, schedule_path):
+        self.file_path = schedule_path
+        options, items = self.extract_options_and_schedule_items(schedule_path)
+
+        self.loaded_options = options
+        self.options = OrderedDict()
+        self.options.update(DEFAULTS)
+        self.options.update(options)
+
+        self.items = items
+
+    @classmethod
+    def extract_options_and_schedule_items(klass, data):
+        schedule = yaml_load_all(data)
+        options = {}
+        items = []
+
+        # Get first item, see if it contains defaults:
+        item = schedule.popleft()
+        if 'defaults' in item:
+            options = item['defaults']
+        else:
+            items.append(item)
+
+        # Everything else is a schedule item.
+        items.extend(schedule)
+
+        return options, items
+
+    def write_schedule(self, fp=None):
+        if fp is None:
+            assert isinstance(self.file_path, six.string_types)
+            fp = self.file_path
+
+        with open(fp, 'w') as fo:
+            fo.write(self.as_yaml())
+
+    def set_option(self, key, value):
+        self.loaded_options[key] = value
+        self.options[key] = value
+
+    def add_ids(self):
+        for item in self.items:
+            if 'id' not in item:
+                item['id'] = uuid.uuid1().hex
+
+    def as_stream(self):
+        stream = [{'defaults': self.loaded_options}]
+        stream.extend(self.items)
+        return stream
+
+    def as_yaml(self):
+        return yaml_dump_all(self.as_stream())
+
+
+class Schedule(object):
+    def __init__(self, options, items):
+        self.options = options
+        self.items = items
+
+    def by_id(self, id):
+        try:
+            return next(it.dropwhile(
+                lambda i: i.get('id') != id,
+                self.items
+            ))
+        except StopIteration:
+            raise IndexError(id)
+
+    def next_after_id(self, id):
+        try:
+            gen = it.dropwhile(
+                lambda i: i.get('id') != id,
+                self.items
+            )
+            next(gen)
+            return next(gen)
+        except StopIteration:
+            if self.options['repeat']:
+                return self.items[0]
+            else:
+                raise IndexError(id)
+
+    def get_timezone(self):
+        return pytz.timezone(self.options['timezone'])
+
+    def localize_naive_utc_datetime(self, time):
+        return pytz.UTC.localize(time).astimezone(self.get_timezone())
+
+    def get_now(self):
+        return self.localize_naive_utc_datetime(dt.datetime.utcnow())
+
+
+class StateLoader(object):
+    def __init__(self, state_path):
+        self.state_path = state_path
+        self.state = yaml_load(state_path)
+
+    def write_state(self, fp=None):
+        if fp is None:
+            assert isinstance(self.state_path, six.string_types)
+            fp = self.state_path
+
+        with open(fp, 'w') as fo:
+            state = yaml_dump(self.state)
+            fo.write(state)
+
+    def reset(self):
+        self.state.clear()
+
+
+class SENTINEL(object):
+    pass
+
+
+class Scriptter(object):
+    def __init__(self, schedule, state):
+        self.state = state
+        self.schedule = schedule
+        self.calendar = parsedatetime.Calendar()
+
+    def get_scheduled_item(self):
+        scheduled_item = None
+        scheduled_id = self.state.get('scheduled', SENTINEL)
+        if scheduled_id is SENTINEL:
+            scheduled_item = self.schedule.items[0]
+        elif scheduled_id:
+            try:
+                scheduled_item = self.schedule.by_id(scheduled_id)
+            except IndexError:
+                logger.error("Could not find scheduled item %s", scheduled_id)
+
+        return scheduled_item
+
+    def get_scheduled_run_time(self):
+        when = self.state.get('when')
+        if not when:
+            when = self.schedule.get_now()
+        else:
+            when = pytz.UTC.localize(when)
+
+        return when.astimezone(self.schedule.get_timezone())
+
+    def get_next_item_after(self, item):
+        try:
+            return self.schedule.next_after_id(item['id'])
+        except IndexError:
+            return None
+
+    def get_next_run_time(self, item, now=None):
+        ctx = self.get_context(item)
+        delay = ctx['delay']
+        tz = pytz.timezone(ctx['timezone'])
+        if now is None:
+            now = pytz.UTC.localize(dt.datetime.utcnow())
+        if now.tzinfo is None:
+            now = pytz.UTC.localize(now)
+        now = now.astimezone(tz)
+        logger.debug('Using base `now`: %r', now)
+        when = self.calendar.parseDT(
+            delay,
+            sourceTime=now,
+            tzinfo=tz,
+        )[0]
+        return when
+
+    def set_next(self, item, now=None):
+        next_item = self.get_next_item_after(item)
+        if next_item is None:
+            next_id = None
+        else:
+            next_id = next_item['id']
+
+        self.state['scheduled'] = next_id
+        self.state['when'] = (
+            None if next_id is None
+            else self.get_next_run_time(next_item, now=now))
+
+    def get_context(self, item):
+        ctx = dict(self.schedule.options)
+        ctx.update(item)
+        return ctx
+
+    def get_commands(self, item):
+        ctx = self.get_context(item)
+        cmd = ctx['cmd']
+        if isinstance(cmd, six.string_types):
+            commands = [cmd]
+        elif isinstance(cmd, Iterable):
+            commands = list(cmd)
+
+        return [command.format(**ctx) for command in commands]
+
+    def run(self, dry_run=False):
+        item = self.get_scheduled_item()
+        when = self.get_scheduled_run_time()
+        now = self.schedule.get_now()
+
+        if item is None:
+            logger.warning("Nothing to do!")
+        elif when > now:
+            logger.warning("Will run `%s` at %s", item['id'], when.isoformat())
+        else:
+            self.set_next(item)
+            logger.debug('Running with item: %s', pprint.pformat(item))
+
+            for command in self.get_commands(item):
+                command = shlex.split(command)
+                logger.info("Running command: %r", command)
+                if not dry_run:
+                    result = subprocess.check_output(command)
+                logger.info("Result was: %s", result)
+
+    def check(self):
+        formatter = '%b %d, %Y at %X'
+        now = self.schedule.get_now()
+        print("%s %s -- (start)" % (now.strftime(formatter), now.tzinfo))
+        for item in self.schedule.items:
+            print('-----')
+            delay = self.get_context(item)['delay']
+            when = self.get_next_run_time(item, now=now)
+            print("%s -- (%s)" % (when.strftime(formatter), delay))
+            for command in self.get_commands(item):
+                print(command)
+            now = when
+        print('-----')
+
+
+def main():   # pragma: no cover
+    arguments = docopt(__doc__, version='Scriptter {}'.format(VERSION))
+    if arguments.get('--verbose'):
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    logger.debug("Received arguments: %s", pprint.pformat(arguments))
+    loaded_schedule = ScheduleLoader(arguments['<schedule>'])
+    loaded_schedule.add_ids()
+    loaded_schedule.write_schedule()
+
+    schedule = Schedule(loaded_schedule.options, loaded_schedule.items)
+
+    state_path = arguments.get('<state-path>', './state.yml')
+    state = StateLoader(state_path)
+
+    if arguments['--reset']:
+        state.reset()
+        state.write_state()
+
+    scriptter = Scriptter(schedule, state.state)
+
+    if arguments['run'] or arguments['trial']:
+        scriptter.run(dry_run=arguments['trial'])
+        state.write_state()
+    elif arguments['check']:
+        scriptter.check()
+
+
+if __name__ == '__main__':   # pragma: no cover
+    main()
---
+++ tests.py
@@ -0,0 +1,463 @@
+# -*- coding: utf-8 -*-
+from collections import deque, OrderedDict
+import datetime as dt
+import mock
+import unittest
+import pep8
+import tempfile
+
+from ensure import ensure
+from path import path
+import pytz
+
+import scriptter
+
+
+PATH = path(__file__).abspath().dirname()
+DATA = PATH / 'test-data'
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(config_file=PATH / '.pep8')
+        result = pep8style.check_files(['scriptter.py', 'tests.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class YamlTests(unittest.TestCase):
+    def setUp(self):
+        self.docs = DATA / 'simple_document_stream.yaml'
+        self.doc = DATA / 'simple_document.yaml'
+
+    def test_it_should_load_all_documents_from_a_string(self):
+        result = scriptter.yaml_load_all(self.docs.text())
+        ensure(result).is_a(deque).of(list)
+        ensure(result).has_length(2)
+
+    def test_it_should_load_all_documents_from_a_filepath(self):
+        result = scriptter.yaml_load_all(self.docs)
+        ensure(result).is_a(deque).of(list)
+        ensure(result).has_length(2)
+
+    def test_it_should_load_all_documents_from_a_file(self):
+        with open(self.docs) as fi:
+            result = scriptter.yaml_load_all(fi)
+        ensure(result).is_a(deque).of(list)
+        ensure(result).has_length(2)
+
+    def test_it_should_load_a_document_from_a_string(self):
+        result = scriptter.yaml_load(self.doc.text())
+        ensure(result).is_a(dict)
+        ensure(result).has_key('foo').whose_value.equals('bar')  # noqa
+        ensure(result).has_key('baz').whose_value.equals('blah')  # noqa
+
+    def test_it_should_load_a_document_from_a_filepath(self):
+        result = scriptter.yaml_load(self.doc)
+        ensure(result).is_a(dict)
+        ensure(result).has_key('foo').whose_value.equals('bar')  # noqa
+        ensure(result).has_key('baz').whose_value.equals('blah')  # noqa
+
+    def test_it_should_load_a_document_from_a_file(self):
+        with open(self.doc) as fi:
+            result = scriptter.yaml_load(fi)
+        ensure(result).is_a(dict)
+        ensure(result).has_key('foo').whose_value.equals('bar')  # noqa
+        ensure(result).has_key('baz').whose_value.equals('blah')  # noqa
+
+    def test_it_should_dump_a_document_to_a_string(self):
+        result = scriptter.yaml_dump(
+            OrderedDict([('foo', 'bar'), ('baz', 'blah')])
+        )
+        ensure(result).equals(self.doc.text())
+
+    def test_it_should_dump_a_stream_of_documents_to_a_string(self):
+        result = scriptter.yaml_dump_all(
+            [
+                ['foo'],
+                ['bar'],
+            ]
+        )
+        ensure(result).equals(self.docs.text())
+
+
+class ScheduleExtractionWithDefaultsTests(unittest.TestCase):
+    def setUp(self):
+        result = scriptter.ScheduleLoader.extract_options_and_schedule_items(
+            DATA / 'schedule_with_defaults.yaml'
+        )
+        self.result = result
+
+    def test_it_extracts_options_and_a_schedule_from_a_stream(self):
+        ensure(self.result).is_a(tuple)
+        ensure(self.result).has_length(2)
+        ensure(self.result[0]).is_a(dict)
+        ensure(self.result[1]).is_a(list)
+
+    def test_it_extracts_options_from_a_stream(self):
+        ensure(self.result[0]).has_length(2)
+        ensure(
+            self.result[0]
+        ).has_key('delay').whose_value.equals('30min')  # noqa
+        ensure(
+            self.result[0]
+        ).has_key('timezone').whose_value.equals('US/Eastern')  # noqa
+
+    def test_it_extracts_a_schedule_from_a_stream(self):
+        ensure(self.result[1]).is_a(list).of(dict)
+        ensure(self.result[1]).has_length(3)
+
+
+class ScheduleExtractionWithoutDefaultsTests(unittest.TestCase):
+    def setUp(self):
+        result = scriptter.ScheduleLoader.extract_options_and_schedule_items(
+            DATA / 'schedule_without_defaults.yaml'
+        )
+        self.result = result
+
+    def test_it_extracts_options_and_a_schedule_from_a_stream(self):
+        ensure(self.result).is_a(tuple)
+        ensure(self.result).has_length(2)
+        ensure(self.result[0]).is_a(dict)
+        ensure(self.result[1]).is_a(list)
+
+    def test_it_extracts_options_from_a_stream(self):
+        ensure(self.result[0]).is_empty()
+
+    def test_it_extracts_a_schedule_from_a_stream(self):
+        ensure(self.result[1]).is_a(list).of(dict)
+        ensure(self.result[1]).has_length(3)
+
+
+class ScheduleLoaderOptionsTests(unittest.TestCase):
+    def test_it_should_load_default_options(self):
+        loader = scriptter.ScheduleLoader(
+            DATA / 'schedule_without_defaults.yaml'
+        )
+        options = loader.options
+        ensure(options).is_a(dict)
+        ensure(options).equals(scriptter.DEFAULTS)
+
+    def test_it_should_mask_default_options_with_loaded_options(self):
+        loader = scriptter.ScheduleLoader(
+            DATA / 'schedule_with_defaults.yaml'
+        )
+        options = loader.options
+        ensure(options).is_a(dict)
+        ensure(
+            options
+        ).has_key('delay').whose_value.equals('30min')  # noqa
+        ensure(
+            options
+        ).has_key('timezone').whose_value.equals('US/Eastern')  # noqa
+
+
+class ScheduleLoaderOperationsTests(unittest.TestCase):
+    def setUp(self):
+        tmpdir = self.tmpdir = path(tempfile.mkdtemp())
+        schedule = DATA / 'schedule_with_defaults.yaml'
+        dest = self.schedule_path = tmpdir / schedule.name
+        schedule.copy(dest)
+        self.loader = scriptter.ScheduleLoader(self.schedule_path)
+
+    def tearDown(self):
+        self.tmpdir.rmtree_p()
+
+    def test_it_should_add_ids_to_a_schedule(self):
+        for item in self.loader.items:
+            ensure(item).does_not_contain('id')
+
+        self.loader.add_ids()
+
+        for item in self.loader.items:
+            ensure(item).contains('id')
+
+    def test_it_should_write_changes_to_a_schedule(self):
+        self.loader.add_ids()
+        self.loader.write_schedule()
+
+        new_loader = scriptter.ScheduleLoader(self.schedule_path)
+        for item in new_loader.items:
+            ensure(item).contains('id')
+
+    def test_it_should_not_write_changes_to_options(self):
+        self.loader.options['foo'] = 'bar'
+        self.loader.write_schedule()
+
+        new_loader = scriptter.ScheduleLoader(self.schedule_path)
+        ensure(new_loader.options).does_not_contain('foo')
+
+    def test_it_should_write_changes_to_loaded_options(self):
+        self.loader.set_option('foo', 'bar')
+        self.loader.write_schedule()
+
+        new_loader = scriptter.ScheduleLoader(self.schedule_path)
+        ensure(
+            new_loader.options
+        ).has_key('foo').whose_value.equals('bar')  # noqa
+
+
+class ScheduleConstructorTests(unittest.TestCase):
+    def setUp(self):
+        self.options, self.items = options, items = {}, []
+        self.schedule = scriptter.Schedule(options, items)
+
+    def test_it_should_instantiate_with_options(self):
+        ensure(self.schedule.options).is_(self.options)
+
+    def test_it_should_instantiate_with_items(self):
+        ensure(self.schedule.items).is_(self.items)
+
+
+class ScheduleOperationsTests(unittest.TestCase):
+    def setUp(self):
+        loaded = scriptter.ScheduleLoader(
+            DATA / 'schedule_with_defaults_and_ids.yaml'
+        )
+
+        self.schedule = scriptter.Schedule(loaded.options, loaded.items)
+
+    def test_it_should_get_items_by_id(self):
+        item = self.schedule.by_id('36292ccff3f811e4889bc82a1417f375')
+        ensure(item).is_a(dict)
+        ensure(item).has_key('say').whose_value.equals('Hello, world!')  # noqa
+
+    def test_getting_a_nonexistent_items_by_id_raises_an_IndexError(self):
+        ensure(self.schedule.by_id).called_with('foo').raises(IndexError)
+
+    def test_it_should_get_the_next_item_after_an_id(self):
+        item = self.schedule.next_after_id('36292ccff3f811e4889bc82a1417f375')
+        ensure(item).is_a(dict)
+        ensure(item).has_key('say').whose_value.equals('Hey, @eykd!')  # noqa
+
+    def test_it_should_wrap_around_when_getting_the_next_item_after_an_id(self):
+        item = self.schedule.next_after_id('4156347af3f811e4a134c82a1417f375')
+        ensure(item).is_a(dict)
+        ensure(item).has_key('say').whose_value.equals('Hello, world!')  # noqa
+
+    def test_it_should_not_wrap_around_when_repeat_is_False(self):
+        self.schedule.options['repeat'] = False
+        ensure(
+            self.schedule.next_after_id
+        ).called_with(
+            '4156347af3f811e4a134c82a1417f375'
+        ).raises(IndexError)
+
+    def test_it_should_obtain_the_timezone(self):
+        ensure(
+            self.schedule.get_timezone
+        ).called_with().equals(pytz.timezone('US/Eastern'))
+
+
+class StateLoaderOperationsTests(unittest.TestCase):
+    def setUp(self):
+        tmpdir = self.tmpdir = path(tempfile.mkdtemp())
+        state = DATA / 'test_state.yaml'
+        dest = self.state_path = tmpdir / state.name
+        state.copy(dest)
+        self.loader = scriptter.StateLoader(self.state_path)
+
+    def tearDown(self):
+        self.tmpdir.rmtree_p()
+
+    def test_it_should_load_the_state(self):
+        state = self.loader.state
+        ensure(state).has_key(  # noqa
+            'scheduled'
+        ).whose_value.equals('ac8cd482f36b11e49c68c82a1417f375')
+
+        expected = dt.datetime(2015, 5, 5, 20, 7, 31)
+        ensure(state).has_key(  # noqa
+            'when'
+        ).whose_value.equals(expected)
+
+    def test_it_should_write_changes_to_state(self):
+        self.loader.state['next'] = 'foo'
+        self.loader.write_state()
+
+        new_loader = scriptter.StateLoader(self.state_path)
+        ensure(new_loader.state).has_key('next'  # noqa
+        ).whose_value.equals('foo')
+
+    def test_it_should_reset_state(self):
+        self.loader.reset()
+        ensure(self.loader.state).is_empty()
+
+
+class ScriptterConstructorTests(unittest.TestCase):
+    def setUp(self):
+        self.options, self.items = options, items = {}, []
+        self.schedule = scriptter.Schedule(options, items)
+        self.state = {}
+        self.scriptter = scriptter.Scriptter(self.schedule, self.state)
+
+    def test_it_should_instantiate_with_options(self):
+        ensure(self.schedule.options).is_(self.options)
+
+    def test_it_should_instantiate_with_items(self):
+        ensure(self.schedule.items).is_(self.items)
+
+
+class ScriptterOperationsTests(unittest.TestCase):
+    def setUp(self):
+        loaded = scriptter.ScheduleLoader(
+            DATA / 'schedule_with_defaults_and_ids.yaml'
+        )
+
+        self.schedule = scriptter.Schedule(loaded.options, loaded.items)
+        self.state = {}
+        self.scriptter = scriptter.Scriptter(self.schedule, self.state)
+
+    def test_it_should_get_the_first_scheduled_item(self):
+        ensure(
+            self.scriptter.get_scheduled_item
+        ).called_with().is_(self.schedule.items[0])
+
+    def test_it_should_get_the_saved_scheduled_item(self):
+        self.scriptter.state['scheduled'] = '3d13091cf3f811e4a8edc82a1417f375'
+        ensure(
+            self.scriptter.get_scheduled_item
+        ).called_with().is_(
+            self.schedule.by_id('3d13091cf3f811e4a8edc82a1417f375')
+        )
+
+    def test_it_should_log_an_error_if_it_cant_get_a_scheduled_item(self):
+        self.scriptter.state['scheduled'] = 'foo'
+        with mock.patch('scriptter.logger.error') as patched:
+            ensure(
+                self.scriptter.get_scheduled_item
+            ).called_with().is_none()
+            ensure(patched.call_count).equals(1)
+
+    def test_it_should_get_utcnow_for_the_scheduled_run_time(self):
+        utcnaw = dt.datetime(2015, 12, 25, 13, 57)
+        utc_localized = pytz.UTC.localize(utcnaw)
+        localized = utc_localized.astimezone(self.schedule.get_timezone())
+        fake_datetime = mock.Mock(utcnow=mock.Mock(return_value=utcnaw))
+        with mock.patch('datetime.datetime', fake_datetime):
+            ensure(
+                self.scriptter.get_scheduled_run_time
+            ).called_with().equals(
+                localized
+            )
+
+    def test_it_should_get_the_next_item_after(self):
+        item = self.schedule.by_id('36292ccff3f811e4889bc82a1417f375')
+        expected = self.schedule.by_id('3d13091cf3f811e4a8edc82a1417f375')
+        ensure(
+            self.scriptter.get_next_item_after
+        ).called_with(
+            item
+        ).is_(expected)
+
+    def test_it_should_return_None_when_theres_no_item_after(self):
+        self.schedule.options['repeat'] = False
+        item = self.schedule.by_id('4156347af3f811e4a134c82a1417f375')
+        ensure(
+            self.scriptter.get_next_item_after
+        ).called_with(
+            item
+        ).is_none()
+
+    def test_it_should_get_a_datetime_as_next_runtime(self):
+        result = self.scriptter.get_next_run_time({})
+
+        ensure(result).is_a(dt.datetime)
+
+    def test_it_should_get_the_default_delay_as_next_runtime(self):
+        utcnaw = dt.datetime(2015, 12, 25, 13, 53)
+        # US/Eastern time and 30 minutes later:
+        expected = pytz.timezone('US/Eastern').localize(
+            dt.datetime(2015, 12, 25, 9, 23)
+        )
+        result = self.scriptter.get_next_run_time({}, now=utcnaw)
+
+        ensure(result).equals(expected)
+
+    def test_it_should_save_the_next_scheduled_item(self):
+        item = self.schedule.by_id('3d13091cf3f811e4a8edc82a1417f375')
+        self.scriptter.set_next(item)
+        ensure(
+            self.scriptter.state
+        ).has_key('scheduled').whose_value.equals(  # noqa
+            '4156347af3f811e4a134c82a1417f375'
+        )
+
+    def test_it_should_save_the_next_scheduled_time(self):
+        utcnaw = dt.datetime(2015, 12, 25, 13, 53)
+        # US/Eastern time and 30 seconds later:
+        expected = pytz.timezone('US/Eastern').localize(
+            dt.datetime(2015, 12, 25, 8, 53, 30)
+        )
+        item = self.schedule.by_id('36292ccff3f811e4889bc82a1417f375')
+        self.scriptter.set_next(item, now=utcnaw)
+        ensure(
+            self.scriptter.state
+        ).has_key('when').whose_value.equals(  # noqa
+            expected
+        )
+
+    def test_it_should_save_None_if_no_more_scheduled_items(self):
+        self.schedule.options['repeat'] = False
+        item = self.schedule.by_id('4156347af3f811e4a134c82a1417f375')
+        self.scriptter.set_next(item)
+        ensure(
+            self.scriptter.state
+        ).has_key('scheduled').whose_value.is_none()  # noqa
+
+    def test_it_should_create_item_context(self):
+        item = self.schedule.by_id('4156347af3f811e4a134c82a1417f375')
+        expected = dict()
+        expected.update(scriptter.DEFAULTS)
+        expected.update(self.schedule.options)
+        expected.update(item)
+
+        ctx = self.scriptter.get_context(item)
+
+        ensure(ctx).equals(expected)
+
+    def test_it_should_render_a_command_to_run(self):
+        item = self.schedule.by_id('4156347af3f811e4a134c82a1417f375')
+        result = self.scriptter.get_commands(item)
+        ensure(result).is_a(list).of(str)
+        ensure(result).equals(
+            [('echo @somebodyelse says: '
+              'Yo @worldsenoughstudios you know I can\'t be beat; '
+              'I heard you like twitter so I put some wit in your tweet!')]
+        )
+
+    def test_it_should_render_commands_to_run(self):
+        self.schedule.options['cmd'] = [
+            'echo @{as} says: {say}',
+            'echo with a delay of {delay}'
+        ]
+        item = self.schedule.by_id('4156347af3f811e4a134c82a1417f375')
+        result = self.scriptter.get_commands(item)
+        ensure(result).is_a(list).of(str)
+        ensure(result).equals(
+            [
+                ('echo @somebodyelse says: '
+                 'Yo @worldsenoughstudios you know I can\'t be beat; '
+                 'I heard you like twitter so I put some wit in your tweet!'),
+                'echo with a delay of next morning 8am',
+            ]
+        )
+
+    def test_it_should_run(self):
+        self.scriptter.state['next'] = '36292ccff3f811e4889bc82a1417f375'
+        self.scriptter.state['when'] = dt.datetime.now() - dt.timedelta(60)
+        with mock.patch('subprocess.check_output') as patched:
+            self.scriptter.run()
+            ensure(patched.call_count).equals(1)
+            ensure(patched.call_args[0][0]).equals(
+                ['echo', '@eykd', 'says:', 'Hello,', 'world!']
+            )
+
+    def test_it_should_not_run_if_nothing_to_run(self):
+        self.schedule.options['repeat'] = False
+        self.scriptter.state['scheduled'] = None
+        with mock.patch('subprocess.check_output') as patched:
+            self.scriptter.run()
+            ensure(patched.call_count).equals(0)
